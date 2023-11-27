962309258f9f3ce58909c34e0ff2226fb3472b00
Nabil Kashyap <nkashyap@getty.edu>
2023-11-26 19:46:53 -0800

    adds simple esbuild bundling

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 const Image = require("@11ty/eleventy-img");
 const sass = require("sass");
 const path = require("path");
+const esbuild = require("esbuild");
 
 // add markdown support
 const mdOptions = {
@@ -65,7 +66,6 @@ module.exports = (eleventyConfig) => {
   // Copy the "assets" directory to the compiled "_site" folder.
   eleventyConfig.addPassthroughCopy("assets/favicon");
   eleventyConfig.addPassthroughCopy("assets/font");
-  eleventyConfig.addPassthroughCopy("assets/js");
   eleventyConfig.addPassthroughCopy("admin");
 
   // for prefix path as needed for relative paths
@@ -75,24 +75,34 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addTemplateFormats("scss");
   // Creates the extension for use
   eleventyConfig.addExtension("scss", {
-    compileOptions: {
-      cache: false,
-      permalink: (_inputContent, inputPath) => (_data) => {
-        let parsed = path.parse(inputPath);
-        if (parsed.name.startsWith("_")) return; // Ignore partials
-        return `assets/css/${parsed.name}.css`;
-      },
-    },
-    getData: async () => ({ eleventyExcludeFromCollections: true }),
+    outputFileExtension: "css",
     compile: (inputContent, inputPath) => {
-      let parsed = path.parse(inputPath);
-      if (parsed.name.startsWith("_")) return; // Ignore partials
+      if (inputPath !== "./assets/css/main.scss") return;
       let result = sass.compileString(inputContent, {
         style: "expanded",
         sourceMap: false,
-        loadPaths: [parsed.dir || ".", eleventyConfig.dir.includes, "_sass"],
+        loadPaths: ["_sass"],
       });
-      return async (_data) => result.css;
+      return async () => result.css;
+    },
+  });
+
+  // esbuild bundle js
+  eleventyConfig.addTemplateFormats("js");
+  eleventyConfig.addExtension("js", {
+    outputFileExtension: "js",
+    compile: async (inputContent, inputPath) => {
+      if (inputPath !== "./assets/js/index.js") return; // only include entry file
+      return async () => {
+        let result = await esbuild.build({
+          target: "es2020",
+          entryPoints: [inputPath],
+          minify: true,
+          bundle: true,
+          write: false,
+        });
+        return result.outputFiles[0].text;
+      };
     },
   });
 
