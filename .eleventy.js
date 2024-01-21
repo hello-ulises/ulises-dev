2f45d546305dc7af174b1824cb528bd30fb0de14
Nabil Kashyap <nkashyap@getty.edu>
2024-01-21 11:05:40 -0800

    move config files

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,17 @@
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 const Image = require("@11ty/eleventy-img");
-const sass = require("sass");
-const path = require("path");
+const fs = require("fs-extra");
+const esbuild = require("esbuild");
+const { sassPlugin } = require("esbuild-sass-plugin");
+const ColorThief = require("colorthief");
 
-// add markdown support
+const EXCLUDE_IMG_TYPE = ["gif"];
+// valid collection item "types" set in tags
+const ITEM_TYPES = ["events", "projects"];
+// which title to skip in rendering navigation
+const NAV_TITLES_TO_SKIP = ["home"];
+
+// set markdown it options
 const mdOptions = {
   html: true,
   breaks: true,
@@ -11,17 +19,50 @@ const mdOptions = {
   typographer: true,
 };
 const markdownIt = require("markdown-it");
-var markdownItAttrs = require("markdown-it-attrs");
+// adds support for inline html attributes (i.e., {.class} )
+let markdownItAttrs = require("markdown-it-attrs");
 const md = new markdownIt(mdOptions);
 md.use(markdownItAttrs);
 
 module.exports = (eleventyConfig) => {
-  /**
-   * Global options
-   */
+  // search index
+  eleventyConfig.addGlobalData("searchIndex", []);
+  eleventyConfig.addGlobalData("predominantColors", {});
 
+  // read in dominant colors data before build
+  eleventyConfig.on("eleventy.before", () => {
+    const filePath = `${eleventyConfig.dir.input}/predominant-colors.json`;
+    if (fs.existsSync(filePath)) {
+      const data = fs.readJsonSync(filePath, { flag: "r" });
+      eleventyConfig.globalData.predominantColors = data;
+    }
+  });
+
+  /**
+   * Render settings for markdown parser
+   */
   // markdown
   eleventyConfig.setLibrary("md", md);
+  /* search index */
+  md.core.ruler.before("block", "raw", function replace(state) {
+    // add prerendered content to search index
+    if (
+      state.env.published &&
+      state.env.tags &&
+      state.env.tags.includes("index")
+    ) {
+      const urlPath = eleventyConfig.getFilter("urlPath")(state.env);
+      if (urlPath) {
+        let raw = state.src.replace(/\n/g, "");
+        eleventyConfig.globalData.searchIndex.map((e) => {
+          if (e.urlPath == urlPath) {
+            e.content = raw;
+          }
+          return e;
+        });
+      }
+    }
+  });
   // replace image parser with 11ty image plugin
   eleventyConfig.amendLibrary("md", (mdLib) => {
     mdLib.renderer.rules.image = (
@@ -37,8 +78,8 @@ module.exports = (eleventyConfig) => {
       const imgSrc = eleventyConfig.dir.input + img.src;
       // image plugin options
       const imgOptions = {
-        widths: [300, 600],
-        urlPath: eleventyConfig.dir.images,
+        widths: [300, 600, 800],
+        urlPath: `/${eleventyConfig.dir.images}`,
         outputDir: `${eleventyConfig.dir.output}/${eleventyConfig.dir.images}`,
       };
       // markdown-it doesn't work with async so use it synchronously
@@ -49,7 +90,7 @@ module.exports = (eleventyConfig) => {
         ...img,
         whitespaceMode: "inline",
         // required for multiple width
-        sizes: [300, 600],
+        sizes: [300, 600, 800],
       });
 
       return imageMarkup;
@@ -59,67 +100,184 @@ module.exports = (eleventyConfig) => {
   });
 
   /**
-   * Folders and extensions
+   * File processing
    */
-
   // Copy the "assets" directory to the compiled "_site" folder.
   eleventyConfig.addPassthroughCopy("assets/favicon");
   eleventyConfig.addPassthroughCopy("assets/font");
   eleventyConfig.addPassthroughCopy("assets/js");
+  eleventyConfig.addPassthroughCopy("assets/img/peace_small.png");
   eleventyConfig.addPassthroughCopy("admin");
 
   // for prefix path as needed for relative paths
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
-  // thanks to https://github.com/GrimLink/eleventy-plugin-sass
-  eleventyConfig.addTemplateFormats("scss");
-  // Creates the extension for use
-  eleventyConfig.addExtension("scss", {
-    compileOptions: {
-      cache: false,
-      permalink: (_inputContent, inputPath) => (_data) => {
-        let parsed = path.parse(inputPath);
-        if (parsed.name.startsWith("_")) return; // Ignore partials
-        return `assets/css/${parsed.name}.css`;
+  /**
+   * Process image using sharp plugin and return image metadata
+   */
+  eleventyConfig.addAsyncFilter("processImage", async (data) => {
+    const featuredImg = data.featured_img;
+    const urlPath = data.page.url;
+
+    if (!data.published || !featuredImg || !urlPath) {
+      return undefined;
+    }
+
+    const metadata = await Image(`.${featuredImg}`, {
+      widths: [400, 800],
+      urlPath: `/${eleventyConfig.dir.images}`,
+      outputDir: `./${eleventyConfig.dir.output}/${eleventyConfig.dir.images}`,
+      sharpOptions: {
+        animated: true,
       },
-    },
-    getData: async () => ({ eleventyExcludeFromCollections: true }),
-    compile: (inputContent, inputPath) => {
-      let parsed = path.parse(inputPath);
-      if (parsed.name.startsWith("_")) return; // Ignore partials
-      let result = sass.compileString(inputContent, {
-        style: "expanded",
-        sourceMap: false,
-        loadPaths: [parsed.dir || ".", eleventyConfig.dir.includes, "_sass"],
-      });
-      return async (_data) => result.css;
-    },
+    });
+
+    // add post to search index
+    let post = {
+      title: data.title,
+      urlPath: urlPath,
+      featuredImg: data.featured_img,
+      thumbnail: {
+        src: metadata.jpeg[0].url,
+        width: metadata.jpeg[metadata.jpeg.length - 1].width,
+        alt: "test",
+        predominantColor:
+          eleventyConfig.globalData.predominantColors[data.featured_img],
+      },
+    };
+    eleventyConfig.globalData.searchIndex.push(post);
+    return metadata;
   });
 
-  /**
-   * Template rendering
-   */
+  // add predominant color filter
+  eleventyConfig.addAsyncFilter("getPredominantColor", async (value) => {
+    let predominantColor = [66, 66, 66];
+
+    if (value && !EXCLUDE_IMG_TYPE.includes(value.slice(-3))) {
+      // check if we have already have the color data saved
+      predominantColor = eleventyConfig.globalData.predominantColors[value];
+      if (predominantColor == undefined) {
+        predominantColor = await ColorThief.getColor(`./${value}`);
+        // add color for new images
+        eleventyConfig.globalData.predominantColors[value] = predominantColor;
+      }
+    }
+
+    return `rgb(${predominantColor[0]},${predominantColor[1]},${predominantColor[2]})`;
+  });
 
   // add category filter
   eleventyConfig.addFilter("byCategory", (collection, category) =>
     collection.filter((e) => e.data.categories == category)
   );
 
-  // add markdown-it liquid md filter using global md lib
-  eleventyConfig.addLiquidFilter("markdownify", (obj) => {
-    return md.render(obj);
+  // add upcoming filter
+  eleventyConfig.addFilter("upcoming", (collection) => {
+    const now = new Date();
+    return collection.filter((e) => {
+      const eventDate = new Date(e.data.event_date || e.data.date);
+      return now < eventDate;
+    });
   });
 
+  // add url path filter for collection item pages
+  // returns string "/item_type/YYYY/slug"
+  eleventyConfig.addFilter("urlPath", (item) => {
+    let urlPath;
+    // need to test for null when using filter in directory js data file
+    const itemType = ITEM_TYPES.filter((e) => item?.tags.includes(e));
+    // check for whether this is an allowed collection item "type"
+    if (itemType.length) {
+      let year = item.event_date || item.date;
+      year = new Date(year).getFullYear();
+      urlPath = `/${itemType[0]}/${year}/${item.page.fileSlug}.html`;
+    }
+    return urlPath;
+  });
+
+  // add markdown-it liquid md filter using global md lib
+  eleventyConfig.addLiquidFilter("markdownify", (data) => {
+    return md.render(data);
+  });
+
+  // whether and which title to display in the nav bar
+  eleventyConfig.addLiquidFilter("navTitle", (title, tags) => {
+    const itemType = ITEM_TYPES.filter((e) => tags && tags.includes(e));
+    let navTitle = itemType[0] || title;
+
+    return NAV_TITLES_TO_SKIP.includes(title) ? undefined : navTitle;
+  });
+
+  // add synthetic collections
+  eleventyConfig.addCollection("pastProjects", function (collection) {
+    const pastProjects = collection.getFilteredByTags("projects");
+    return pastProjects.filter((e) => !e.data.tags.includes("featured"));
+  });
+
+  eleventyConfig.addCollection("pastEvents", function (collection) {
+    const pastEvents = collection.getFilteredByTags("events");
+    const now = new Date();
+    return pastEvents.filter((e) => {
+      const eventDate = new Date(e.data.event_date || e.data.date);
+      return now > eventDate;
+    });
+  });
+
+  // ty! https://hipsterbrown.com/musings/musing/esbuild-with-11ty/
+  eleventyConfig.on("eleventy.after", () => {
+    return esbuild.build({
+      target: "es2020",
+      entryPoints: ["./assets/css/style.scss", "./assets/js/main.js"],
+      outdir: "./dist/assets",
+      minify: false,
+      sourcemap: false,
+      bundle: true,
+      plugins: [sassPlugin()],
+      loader: {
+        ".woff2": "file",
+        ".woff": "file",
+      },
+    });
+  });
+
+  // write search index to file
+  eleventyConfig.on("eleventy.after", () => {
+    fs.writeJsonSync(
+      `./${eleventyConfig.dir.output}/search-index.json`,
+      eleventyConfig.globalData.searchIndex,
+      { flag: "w" }
+    );
+
+    // when using dev server, reset value after writing
+    eleventyConfig.globalData.searchIndex = [];
+  });
+
+  // write dominant colors data to file
+  // we're bootstrapping this data because it isn't dynamic
+  eleventyConfig.on("eleventy.after", () => {
+    fs.writeJsonSync(
+      `./${eleventyConfig.dir.input}/predominant-colors.json`,
+      eleventyConfig.globalData.predominantColors,
+      { flag: "w" }
+    );
+
+    // when using dev server, reset value after writing
+    eleventyConfig.globalData.dominantColors = {};
+  });
+
+  eleventyConfig.addWatchTarget("./assets/css/**/*.scss");
+  eleventyConfig.addWatchTarget("./assets/js/*.js");
   /**
-   * Global settings object
+   * Global settings
    */
   return {
     dir: {
       input: ".",
-      output: "_site",
+      output: "dist",
       layouts: "layouts",
       includes: "includes",
       images: "assets/img",
+      css: "assets/css",
     },
     templateFormats: ["html", "liquid", "md", "njk"],
     markdownTemplateEngine: "njk",
