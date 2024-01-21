d962e681c431796f9551130cfef79f7c05bb995e
Nabil Kashyap <nkashyap@getty.edu>
2024-01-21 11:08:44 -0800

    refactor meta info

---
+++ meta/footer.md
@@ -0,0 +1,16 @@
+---
+tags:
+  - footer
+content:
+  visit:
+    - 1525 N American St, Philadelphia PA 19122
+    - Studio 104
+  hours:
+    - "Wednesday - Friday: 12-7 PM"
+    - "Saturday - Sunday: 12-4 PM"
+  links:
+    - text: email
+      url: "mailto:ulisesbooks@gmail.com"
+    - text: instagram
+      url: https://www.instagram.com/ulisesbooks
+---
--- posts/posts.11tydata.js
+++ posts/posts.11tydata.js
@@ -1,3 +1,21 @@
 module.exports = {
-  tags: ["post"],
+  layout: "post-page",
+  tags: ["index"],
+  eleventyComputed: {
+    eleventyExcludeFromCollections: (data) => !data.published,
+    permalink(data) {
+      const urlPath = this.urlPath(data);
+      return data.published && urlPath ? urlPath : false;
+    },
+    async predominantColor(data) {
+      const predominantColor = await this.getPredominantColor(
+        data.featured_img
+      );
+      return predominantColor;
+    },
+    async imageMetadata(data) {
+      const imageMetadata = await this.processImage(data);
+      return imageMetadata;
+    },
+  },
 };
